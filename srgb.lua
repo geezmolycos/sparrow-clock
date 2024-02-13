9b1fe83a31b26a102ae485e165f9a461bcc055f6
geezmolycos <djy0212@foxmail.com>
2024-02-13 17:31:59 +0800

    修复srgb插值变量名出错

--- srgb.lua
+++ srgb.lua
@@ -87,13 +87,13 @@ end
 
 function srgb.interpolate_in_linear(ratio, c1, c2)
     local r1l, g1l, b1l = srgb.to_linear(c1[1], c1[2], c1[3])
-    local r2l, g2l, b2l = srgb.to_linear(c1[1], c1[2], c1[3])
+    local r2l, g2l, b2l = srgb.to_linear(c2[1], c2[2], c2[3])
     if type(ratio) == 'number' then
-        return srgb.from_linear(r1 * ratio + r2 * (1-ratio), g1 * ratio + g2 * (1-ratio), b1 * ratio + b2 * (1-ratio))
+        return srgb.from_linear(r1l * ratio + r2l * (1-ratio), g1l * ratio + g2l * (1-ratio), b1l * ratio + b2l * (1-ratio))
     else
         local result = {}
         for i, it in ipairs(ratio) do
-            local r, g, b = srgb.from_linear(r1 * it + r2 * (1-it), g1 * it + g2 * (1-it), b1 * it + b2 * (1-it))
+            local r, g, b = srgb.from_linear(r1l * it + r2l * (1-it), g1l * it + g2l * (1-it), b1l * it + b2l * (1-it))
             table.insert(result, {r, g, b})
         end
         return result
